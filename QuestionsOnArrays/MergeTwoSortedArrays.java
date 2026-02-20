ecab2a2cc5e40f23ca1b8ad9dd380ffe1952abd6
Aditi Shukla <aditishukla1676@gmail.com>
2026-02-20 11:14:17 +0530

    Merge Two Sorted Arrays

---
+++ QuestionsOnArrays/MergeTwoSortedArrays.java
@@ -0,0 +1,37 @@
+package QuestionsOnArrays;
+
+public class MergeTwoSortedArrays {
+    public static void main(String[] args){
+        int[] a = {11,33,42,71};
+        int[] b = {26,54,69,81};
+        int m = a.length , n=b.length;
+        int[] c = new int[m+n];
+        int i = 0, j=0, k=0;
+       //merging
+       while(i<a.length && j<b.length){
+         if(a[i]<=b[j]){
+            c[k]=a[i];
+            i++;
+         }
+         else{
+            c[k]=b[j];
+            j++;
+         }
+         k++;
+       }
+       if(i==a.length){ //now take elements from b only
+        while(j<b.length){
+        c[k]=b[j];
+        j++;k++;
+        }
+       }
+       if(j==b.length){
+        while(i<a.length){
+        c[k]=a[i];
+        i++; k++;
+        }
+        for(int ele: c){
+            System.out.print(ele+" ");
+        }       }
+    }
+}
