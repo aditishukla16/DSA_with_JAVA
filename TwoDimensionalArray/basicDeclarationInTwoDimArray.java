c48dfedfe07e4fb03d5a418ac3b21e5ef0e2fcd4
Aditi Shukla <aditishukla1676@gmail.com>
2026-02-27 23:08:24 +0530

    Basic declaration of 2D Array

---
+++ TwoDimensionalArray/basicDeclarationInTwoDimArray.java
@@ -0,0 +1,11 @@
+public class basicDeclarationInTwoDimArray {
+   public static void main(String[] args) {
+    int[][] arr = new int[3][3];
+    //10 20 30
+    //40 50 60
+    //70 80 90
+    arr[0][0]=10;
+    arr[0][1]=20;
+    arr[0][1]=30;
+   } 
+}
