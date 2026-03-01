967adb381c675e210d3c728f3dacca65d8d4fa5d
Aditi Shukla <aditishukla1676@gmail.com>
2026-03-01 21:50:54 +0530

    sum of elements in 2D array

---
+++ TwoDimensionalArray/sumOfElementsin2DArray.java
@@ -0,0 +1,14 @@
+public class sumOfElementsin2DArray {
+    public static void main(String[] args){
+        int[][] arr = {{1,5,6},{2,-9,30},{5,10,3}};
+        int m = arr.length;
+        int n= arr[0].length;
+        int sum = 0;
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+              sum += arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
